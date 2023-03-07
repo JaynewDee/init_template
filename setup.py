9234627a44408a1923c14862fff8bfe7efaa1164
NuminousBlue <jdiehl2236@gmail.com>
2023-03-07 17:01:13 -0600

    init

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from distutils.core import setup
+
+setup(name='New Project',
+      version='1.0',
+      description='_',
+      author='Joshua Newell Diehl',
+      author_email='jdiehl2236@gmail.com',
+      url='_',
+      packages=[''],
+      )
